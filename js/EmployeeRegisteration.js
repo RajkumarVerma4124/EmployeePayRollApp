ce0517ce79747d5d78c167379667d43fb72feab5
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-03-15 11:25:46 +0530

    [Raj] Added:- The event listener to set values of emp

--- js/EmployeeRegisteration.js
+++ js/EmployeeRegisteration.js
@@ -1,7 +1,36 @@
-//Javascript for salary range(UC8)
-const salary = document.querySelector('#salary');
-const outputSal = document.querySelector('#salaryOutput');
-outputSal.textContent = salary.value;
-salary.addEventListener('input', () => {
+//Javascript to add set event listeners on document loading(UC10)
+window.addEventListener('DOMContentLoaded', (event) => {
+    //Setting the name to the employee object for validation
+    const name = document.querySelector('#empName');
+    const nameError = document.querySelector('#errorName');
+    name.addEventListener('input', () => {
+        if (name.value.length == 0) {
+            nameError.textContent = '';
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).empName = name.value;
+            nameError.textContent = '';
+        } catch (e) {
+            nameError.textContent = e;
+        }
+    });
+    //Setting the salary output by checking the salary value(UC10)
+    const salary = document.querySelector('#salary');
+    const outputSal = document.querySelector('#salaryOutput');
     outputSal.textContent = salary.value;
+    salary.addEventListener('input', () => {
+        outputSal.textContent = salary.value;
+    });
+    //Setting the date to the employee object for validation(UC10)
+    const date = document.querySelector('#date');
+    const errorDate = document.querySelector('#errorDate');
+    date.addEventListener('input', function() {
+        try {
+            (new EmployeePayrollData()).startDate = new Date(date);
+            errorDate.textContent = "";
+        } catch (e) {
+            errorDate.textContent = e;
+        }
+    });
 });
